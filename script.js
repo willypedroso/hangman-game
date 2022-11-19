90998c7aeaa97d673047ab7fa543efa04700cf6c
Will Pedroso <willyng.pedroso@gmail.com>
2022-11-19 17:17:58 -0300

    add mistakes div

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
 const words = [
-    "Node",
-    "React",
-    "Npm",
-    "Vue",
-    "Angular",
-    "Javascript",
+    "NODE",
+    "REACT",
+    "NPM",
+    "VUE",
+    "ANGULAR",
+    "JAVASCRIPT",
     "HTML",
     "CSS",
-    "Tailwind"
+    "TAILWIND"
 ];
 
+var letters = [];
+var emptyLetters = [];
+var mistakes = 0;
+
+const res = document.getElementById('res');
+const mistakesDiv = document.getElementById('mistakes');
+
 function start(){
-    let random = Math.floor(Math.random()*words.length);
-    let randomWord = words[random];
-    let letters = randomWord.split("");
+    document.getElementById('main').style.display = "flex";
+    mistakesDiv.style.display = "none";
+    res.innerHTML = "";
+    mistakes = 0;
+
+    var random = Math.floor(Math.random()*words.length);
+    var randomWord = words[random];
+    letters = randomWord.split("");
+    for(i=0;i<letters.length;i++){
+        emptyLetters.push("_ ");
+    };
+
+    console.log(emptyLetters);
     document.getElementById('btnStart').style.display = "none";
-    document.getElementById('res').style.display = "flex";
+    res.style.display = "flex";
     document.getElementById('guess').style.display = "flex";
     document.getElementById('guessLetter').focus();
     console.log(randomWord);
-    letters.map(letter => document.getElementById('res').innerHTML += "_ ");
+    emptyLetters.map(letter => res.innerHTML += letter);
+    document.getElementById('finalMsg').style.display = "none";
+};
+
+function guess(){
+    let guessLetter = (document.getElementById('guessLetter').value).toUpperCase();
+    mistakesDiv.style.display = "block";
+    
+    if(letters.includes(guessLetter)){
+        let spot = letters.indexOf(guessLetter);
+        emptyLetters.splice(spot, 1, `${guessLetter} `);
+        res.innerHTML = "";
+        emptyLetters.map(letter => res.innerHTML += letter);
+        document.getElementById('guessLetter').value = "";
+        document.getElementById('guessLetter').focus();
+    } else {
+        mistakes++
+        if(mistakes == 6){
+            document.getElementById('finalMsg').style.display = "flex";
+            document.getElementById('finalMsg').innerHTML += "YOU LOSE!";
+            document.getElementById('main').style.display = "none";
+        } else {
+            mistakesDiv.innerHTML = `Wrong! You still have ${6-mistakes} tries`;
+            document.getElementById('guessLetter').value = "";
+            document.getElementById('guessLetter').focus();
+        }
+    };
 };
