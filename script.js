501e14bd890abdd61141ded6b2cc9440bcbbe3b7
Will Pedroso <willyng.pedroso@gmail.com>
2022-11-19 10:59:38 -0300

    update

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const words = [
     "HTML",
     "CSS",
     "Tailwind"
-]
+];
 
 function start(){
-    let random = Math.floor(Math.random()*words.length)
-    let randomWord = words[random]
-    let letters = randomWord.split("")
-    document.getElementById('btnStart').style.display = "none"
-    document.getElementById('res').style.display = "flex"
-    document.getElementById('guess').style.display = "flex"
-    console.log(randomWord)
-    letters.map(letter => document.getElementById('res').innerHTML += "_ ")
-}
+    let random = Math.floor(Math.random()*words.length);
+    let randomWord = words[random];
+    let letters = randomWord.split("");
+    document.getElementById('btnStart').style.display = "none";
+    document.getElementById('res').style.display = "flex";
+    document.getElementById('guess').style.display = "flex";
+    console.log(randomWord);
+    letters.map(letter => document.getElementById('res').innerHTML += "_ ");
+};
